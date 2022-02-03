2fe334de68ad94410e5b90e13ae2723e59d5905e
Miguel <miguezaga2@hotmail.com>
2022-02-03 09:02:42 -0500

    terminado

--- dashboard-movies/src/components/App.js
+++ dashboard-movies/src/components/App.js
@@ -1,20 +1,12 @@
 import './../assets/css/app.css'
 import SideBar from './SideBar'
-import TopBar from './TopBar';
-import ContentRowTop from './ContentRowTop';
-import Footer from './Footer'
+import ContentWrapper from './ContentWrapper';
 
 function App() {
   return (
     <div id="wrapper">
       <SideBar />
-      <div id="content-wrapper" className="d-flex flex-column">
-        <div id="content">
-          <TopBar />
-          <ContentRowTop />
-        </div>
-        <Footer />
-      </div>
+      <ContentWrapper />
     </div>
   );
 }
