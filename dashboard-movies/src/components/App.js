25f09ce8cd1211b8c3a59c0956f1a4bebff3dbb9
Miguel <miguezaga2@hotmail.com>
2022-02-03 08:32:59 -0500

    TopBar y ContentRowTop

---
+++ dashboard-movies/src/components/App.js
@@ -0,0 +1,14 @@
+import './../assets/css/app.css'
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+
+function App() {
+  return (
+    <div id="wrapper">
+      <TopBar />
+      <ContentRowTop />
+    </div>
+  );
+}
+
+export default App;
